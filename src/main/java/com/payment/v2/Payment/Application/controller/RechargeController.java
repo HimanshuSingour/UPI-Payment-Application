d2ecf974daed514f46bc5057821fc600ac5be016
Himanshu Singour <singourhimanshu3@gmail.com>
2023-10-28 16:19:12 +0530

    Add Retryable Modules

--- src/main/java/com/payment/v2/Payment/Application/controller/RechargeController.java
+++ src/main/java/com/payment/v2/Payment/Application/controller/RechargeController.java
@@ -8,6 +8,9 @@ import com.payment.v2.Payment.Application.entity.RechargePlanes;
 import com.payment.v2.Payment.Application.dto.ServiceProviderRequest;
 import com.payment.v2.Payment.Application.entity.ServiceProvider;
 import com.payment.v2.Payment.Application.service.TelecomService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,57 +18,70 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/telecom/v10/pro")
+@Slf4j
 public class RechargeController {
 
     @Autowired
     private TelecomService telecomService;
 
-
     // for Developers
     @PostMapping("/save/v7/provider")
-    ResponseEntity<ServiceProvider> addProvider(@RequestBody ServiceProvider netBankingRequest){
+    ResponseEntity<ServiceProvider> addProvider(@RequestBody ServiceProvider netBankingRequest) {
+        log.info("Received request to add a provider: {}", netBankingRequest);
         ServiceProvider response = telecomService.addServiceProvides(netBankingRequest);
-        return new ResponseEntity<ServiceProvider>(response , HttpStatus.ACCEPTED);
+        log.info("Provider added: {}", response);
+        return new ResponseEntity<ServiceProvider>(response, HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/add/v8/plans")
-    ResponseEntity<RechargePlanes> addRechargesPacks(@RequestBody ServiceProviderRequest serviceProviderRequest){
+    ResponseEntity<RechargePlanes> addRechargesPacks(@RequestBody ServiceProviderRequest serviceProviderRequest) {
+        log.info("Received request to add recharge plans: {}", serviceProviderRequest);
         RechargePlanes Re = telecomService.addRechargePlan(serviceProviderRequest);
-        return new ResponseEntity<RechargePlanes>(Re , HttpStatus.ACCEPTED);
+        log.info("Recharge plans added: {}", Re);
+        return new ResponseEntity<RechargePlanes>(Re, HttpStatus.ACCEPTED);
     }
 
     // users
     @GetMapping("/get/v8/plans/{providedId}")
-    ResponseEntity<List<RechargePlanes>> getRechargeById(@PathVariable String providedId){
+    ResponseEntity<List<RechargePlanes>> getRechargeById(@PathVariable String providedId) {
+        log.info("Received request to get recharge plans for provider ID: {}", providedId);
         List<RechargePlanes> t = telecomService.getAllRechargePlansById(providedId);
+        log.info("Recharge plans retrieved: {}", t);
         return new ResponseEntity<>(t, HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/get/v8/plans/recharge/now")
-    ResponseEntity<RechargeResponse> rechargeNow(@RequestBody RechargeRequest rechargeRequest){
-       RechargeResponse response = telecomService.rechargeNow(rechargeRequest);
+    @Retry(name = "AccountInformationService", fallbackMethod = "AccountInformationServiceFallBack")
+    ResponseEntity<RechargeResponse> rechargeNow(@RequestBody RechargeRequest rechargeRequest) {
+        log.info("Received request to recharge now: {}", rechargeRequest);
+        RechargeResponse response = telecomService.rechargeNow(rechargeRequest);
+        log.info("Recharge completed: {}", response);
         return new ResponseEntity<RechargeResponse>(response, HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/packs/above/amount")
-    ResponseEntity<List<RechargePlanes>> giveByAboveAmount(@RequestBody ProviderRequest providerRequest){
+    ResponseEntity<List<RechargePlanes>> giveByAboveAmount(@RequestBody ProviderRequest providerRequest) {
+        log.info("Received request to get recharge plans above a certain amount: {}", providerRequest);
         List<RechargePlanes> response = telecomService.getAllRechargeAboveTheGivenAmount(providerRequest);
+        log.info("Recharge plans above the given amount retrieved: {}", response);
         return new ResponseEntity<List<RechargePlanes>>(response, HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/packs/below/amount")
-    ResponseEntity<List<RechargePlanes>> giveByBelowAmount(@RequestBody ProviderRequest providerRequest){
+    ResponseEntity<List<RechargePlanes>> giveByBelowAmount(@RequestBody ProviderRequest providerRequest) {
+        log.info("Received request to get recharge plans below a certain amount: {}", providerRequest);
         List<RechargePlanes> response = telecomService.getAllRechargeAboveTheGivenAmount(providerRequest);
+        log.info("Recharge plans below the given amount retrieved: {}", response);
         return new ResponseEntity<List<RechargePlanes>>(response, HttpStatus.ACCEPTED);
     }
 
     @GetMapping("/packs/activating/info/{planId}")
-    ResponseEntity<ActivationInfo> getActivationIngo(@PathVariable String planId){
+    ResponseEntity<ActivationInfo> getActivationInfo(@PathVariable String planId) {
+        log.info("Received request to get activation information for plan ID: {}", planId);
         ActivationInfo response = telecomService.getActivationInfoByRechargePacks(planId);
+        log.info("Activation information retrieved: {}", response);
         return new ResponseEntity<ActivationInfo>(response, HttpStatus.ACCEPTED);
     }
-
 }
---
+++ src/main/java/com/payment/v2/Payment/Application/entity/TransactionHistory.java
@@ -0,0 +1,4 @@
+package com.payment.v2.Payment.Application.entity;
+
+public class TransactionHistory {
+}
